ab1b8d4d599fb16c6b68bb8a771f1753bf3bd455
Santiago Gilabert <santiagogilabert@gmail.com>
2015-09-01 13:16:49 +0000

    Update test with more examples.

--- src/test/java/com/altipeak/safewalk/SafewalkClientImplTest.java
+++ src/test/java/com/altipeak/safewalk/SafewalkClientImplTest.java
@@ -14,7 +14,8 @@ public class SafewalkClientImplTest  extends TestCase
     private static final long  PORT = 8443;
     private static final String AUTHENTICATION_API_ACCESS_TOKEN = "e488913954d95d66114ae680ed7492b3b213f9ac";
     private static final String ADMIN_API_ACCESS_TOKEN = "1237d30e0f29e6e59bb5a27cce1373722c72c749";
-    private static final String USERNAME = "testuser";
+    private static final String INTERNAL_USERNAME = "testinternaluser";
+    private static final String LDAP_USERNAME = "testldapuser@local.com";
     
     private ServerConnectivityHelper serverConnectivityHelper = new ServerConnectivityHelperImpl(HOST, PORT);
     
@@ -35,34 +36,42 @@ public class SafewalkClientImplTest  extends TestCase
         return new TestSuite( SafewalkClientImplTest.class );
     }
 
-    public void testSafewalkClient() throws ConnectivityException {
-        
+    public void testWithInternalUser() throws ConnectivityException {
+        testSafewalkClient(INTERNAL_USERNAME);
+    }
+    
+    public void testWithLdapUser() throws ConnectivityException {
+        testSafewalkClient(LDAP_USERNAME);
+    }
+    
+    private void testSafewalkClient(String username) throws ConnectivityException {
+        System.out.println("\nBEGIN TEST");
         SafewalkClient client = new SafewalkClientImpl(this.serverConnectivityHelper);
-        CreateUserResponse response2 = client.createUser(ADMIN_API_ACCESS_TOKEN, USERNAME, "12345", "Test", "User", "+5491222546985", "test@email.com", "");
+        CreateUserResponse response2 = client.createUser(ADMIN_API_ACCESS_TOKEN, username, "12345", "Test", "User", "+5491222546985", "test@email.com", "");
         System.out.println("CREATE USER RESPONSE : " + response2);
         //
-        AuthenticationResponse response1 = client.authenticate(AUTHENTICATION_API_ACCESS_TOKEN, USERNAME, "12345");
+        AuthenticationResponse response1 = client.authenticate(AUTHENTICATION_API_ACCESS_TOKEN, username, "12345");
         System.out.println("AUTHENTICATION RESPONSE : " + response1);
         //
-        UpdateUserResponse response3 = client.updateUser(ADMIN_API_ACCESS_TOKEN, USERNAME, "+549122254116985", "test111@email.com");
+        UpdateUserResponse response3 = client.updateUser(ADMIN_API_ACCESS_TOKEN, username, "+549122254116985", "test111@email.com");
         System.out.println("UPDATE USER RESPONSE : " + response3);
         //
-        GetUserResponse response4 = client.getUser(ADMIN_API_ACCESS_TOKEN, USERNAME);
+        GetUserResponse response4 = client.getUser(ADMIN_API_ACCESS_TOKEN, username);
         System.out.println("GET USER RESPONSE : " + response4);
         //
-        SetStaticPasswordResponse response5 = client.setStaticPassword(ADMIN_API_ACCESS_TOKEN, USERNAME, "abcde");
+        SetStaticPasswordResponse response5 = client.setStaticPassword(ADMIN_API_ACCESS_TOKEN, username, "abcde");
         System.out.println("SET STATIC PASSWORD RESPONSE : " + response5);
         //
-        AssociateTokenResponse response6 = client.associateToken(ADMIN_API_ACCESS_TOKEN, USERNAME, DeviceType.SESAMI_MOBILE, false, false);
+        AssociateTokenResponse response6 = client.associateToken(ADMIN_API_ACCESS_TOKEN, username, DeviceType.SESAMI_MOBILE, false, false);
         System.out.println("ASSOCIATE TOKEN RESPONSE : " + response6);
         //
-        GetTokenAssociationsResponse response7 = client.getTokenAssociations(ADMIN_API_ACCESS_TOKEN, USERNAME);
+        GetTokenAssociationsResponse response7 = client.getTokenAssociations(ADMIN_API_ACCESS_TOKEN, username);
         System.out.println("GET ASSOCIATIONS RESPONSE : " + response7);
         //
-        DeleteTokenAssociation response8 = client.deleteTokenAssociation(ADMIN_API_ACCESS_TOKEN, USERNAME, DeviceType.getEnum(response7.getAssociations().get(0).getDeviceType()), response7.getAssociations().get(0).getSerialNumber());
+        DeleteTokenAssociation response8 = client.deleteTokenAssociation(ADMIN_API_ACCESS_TOKEN, username, DeviceType.getEnum(response7.getAssociations().get(0).getDeviceType()), response7.getAssociations().get(0).getSerialNumber());
         System.out.println("DELETE ASSOCIATIONS RESPONSE : " + response8);
         //
-        DeleteUserResponse response9 = client.deleteUser(ADMIN_API_ACCESS_TOKEN, USERNAME);
+        DeleteUserResponse response9 = client.deleteUser(ADMIN_API_ACCESS_TOKEN, username);
         System.out.println("DELETE USER RESPONSE : " + response9);
     }
    
