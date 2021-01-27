f1e3f4160cb0123ae55c2da77651fbb69b5592df
Tomas Martin <tmcarluccio@gmail.com>
2021-01-27 16:25:33 -0300

    Added push signature options

--- src/main/java/com/altipeak/safewalk/SafewalkClient.java
+++ src/main/java/com/altipeak/safewalk/SafewalkClient.java
@@ -22,7 +22,6 @@ public interface SafewalkClient {
      * </p>
      * 
      * @since v1.1.10
-     * @param accessToken
      * @param username
      * @param password
      * @return {@link AuthenticationResponse}
@@ -43,39 +42,14 @@ public interface SafewalkClient {
      * </p>
      * 
      * @since v1.1.10
-     * @param accessToken
      * @param username
-     * @param password static password or OTP
-     * @param transactionId it can be used to link the authentication transaction with a previous authentication transaction.
+     * @param password - Static password or OTP
+     * @param transactionId - It can be used to link the authentication transaction with a previous authentication transaction.
      * @return {@link AuthenticationResponse}
      * @throws ConnectivityException
      */
     AuthenticationResponse authenticate(String username, String password, String transactionId) throws ConnectivityException;
     
-    /**
-     * <p>
-     * Creates a user with the given {@code username}, {@code password}, {@code firstName}, {@code lastName}, {@code mobilePhone} and {@code email}.
-     * </p>
-     * <p>
-     * The username will determine if the user is an internal user or an LDAP user. 
-     * <ul>
-     *   <li>If the given username has the format of &lt;username&gt; the user will be created as an internal user (In the database).
-     *   <li>If the given username has the format of &lt;username&gt@&lt;domain&gt; the user will be created in the LDAP with the given domain.
-     * </ul>
-     * </p>
-     * 
-     * @since v1.5
-     * @param accessToken
-     * @param username
-     * @param password
-     * @param firstName
-     * @param lastName
-     * @param mobilePhone
-     * @param email
-     * @param parent
-     * @return {@link CreateUserResponse}
-     * @throws ConnectivityException
-     */
     /**
      * <p>
      * Get's the sessionKey string to sign with QR. 
@@ -89,7 +63,6 @@ public interface SafewalkClient {
      * </p>
      * 
      * @since v1.1.10
-     * @param accessToken
      * @return {@link SessionKeyResponse}
      * @throws ConnectivityException
      */
@@ -108,7 +81,6 @@ public interface SafewalkClient {
      * </p>
      * 
      * @since v1.1.10
-     * @param accessToken
      * @param username
      * @param password
      * @return {@link SessionKeyResponse}
@@ -129,7 +101,6 @@ public interface SafewalkClient {
      * </p>
      * 
      * @since v1.1.10
-     * @param accessToken
      * @return {@link SignatureResponse}
      * @throws ConnectivityException
      */
@@ -148,8 +119,11 @@ public interface SafewalkClient {
      * </p>
      * 
      * @since v1.1.10
-     * @param accessToken
      * @param username
+     * @param hash   
+     * @param data  -  The data to sign. Data or body are required.
+     * @param title -  The title displayed in the mobile device. Optional.
+     * @param body  -  The body of the push. Data or body are required.
      * @return {@link AuthenticationResponse}
      * @throws ConnectivityException
      */
