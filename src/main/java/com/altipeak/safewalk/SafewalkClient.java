17d29bf12c863ea9bc4e2dfbb53c42c31d7dc554
Tomas Martin <tmcarluccio@gmail.com>
2021-02-02 15:40:15 -0300

    QR authentication description comment

--- src/main/java/com/altipeak/safewalk/SafewalkClient.java
+++ src/main/java/com/altipeak/safewalk/SafewalkClient.java
@@ -52,7 +52,7 @@ public interface SafewalkClient {
     
     /**
      * <p>
-     * Get's the sessionKey string to sign with QR. 
+     * Creates the sessionKey string to sign with QR. 
      * </p>
      * <p>
      * The username will determine if the user is an internal user or an LDAP user. 
